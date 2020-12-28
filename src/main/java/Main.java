5939fdf272ce66a329c246e2857a5369d968b15a
Rohit Karunakaran <rohitkarun@gmail.com>
2020-12-28 23:10:34 +0530

    Fixed an Issue
    Fixed the issue where month shows 0 and the year shows a value one greater than the required value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,14 +13,22 @@ import java.util.TimeZone;
 class Main{
     public static void main(String[] args) throws IOException,GeneralSecurityException{
         //Files contains all the files which are placed in folders in the Google Drive
-
+        List<OnlineClassFile>files = StartDrive.getFiles();
         //Function which upates the Database
         // JDBC.updateDatabase();
 
         //Function which shows the showDatabase
-        JDBC.showDatabase();
+          JDBC.showDatabase();
+        /*    for(OnlineClassFile file:files){
+                 System.out.printf("%s (%s) Time : %s Type: %s  \nClick here To View File: %s \nBelongs To parent: %s\n\n",
+                     file.getName(),
+                     file.getId(),
+                     file.getDateCreated().toString(),
+                     file.getType(),
+                     file.getWebViewLink(),
+                     file.getParentName() );
 
-      }
 
-
-      }
+                 }*/
+        }
+}
